54cb5559cd258dd1d9d75c5ca84c329b499f2ecb
Ayushb690 <ayushbarik690@gmail.com>
2025-08-01 22:07:15 +0530

    Add files via upload

---
+++ Week_2/README.md
@@ -0,0 +1,9 @@
+# Todo Mongoose Server
+
+## MongoDB Schema Verification
+
+Below is the screenshot from MongoDB Compass showing the `todos` collection created from the schema after performing a manual add operation:
+
+![MongoDB Compass Screenshot](./screenshots/mongodb-verification.png)
+
+> The `Todo` schema has a required `title` field (String). The server successfully connects to MongoDB and inserts a sample document during startup.
--- Week_2/server.js
+++ Week_2/server.js
@@ -1,1 +1,37 @@
+import dotenv from "dotenv"
 
+dotenv.config()
+
+import express from 'express';
+import mongoose from 'mongoose';
+import Todo from './models/Todo.js';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log('Connected to MongoDB');
+
+    const sampleTodo = new Todo({ title: 'Test Todo' });
+
+    sampleTodo.save()
+        .then(() => console.log('Sample todo added to DB.'))
+        .catch(err => console.error('Error adding sample todo:', err));
+})
+.catch((err) => {
+    console.error('MongoDB connection error:', err);
+});
+
+app.get('/', (req, res) => {
+    res.send('Server is running and MongoDB is connected.');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
